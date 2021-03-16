b0e796e74b2d22ea910990c93595d9e84bcc6299
Agrim <agrim19229@iiitd.ac.in>
2021-03-16 12:27:37 +0530

    add sql connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mysql = require('mysql');
+require('dotenv').config()
 
 const router = express.Router();
+const con = mysql.createConnection({
+    host: "sql6.freemysqlhosting.net",
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
+});
+
 
 router.get("/", (req, res, next) => {
     res.send("HELLO");
 });
+
+router.get("/data", (req, res, next) => {
+    con.connect(err => {
+        if (err) { console.log(err); res.send('ERROR'); }
+        console.log("Connected");
+        res.send("SUCCESSFULLY CONNECTED")
+    }
+    )
+}
+)
 
 module.exports = router;
