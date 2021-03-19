1a45abdf81871a4693d0461c133eb78e1247b09b
Agrim <agrim19229@iiitd.ac.in>
2021-03-19 22:15:56 +0530

    add payment methods route

--- routes/user.js
+++ routes/user.js
@@ -11,6 +11,18 @@ router.get('/:id/orders', (req, res, next) => {
         console.log(e);
         return res.send("ERROR");
     }
-})
+});
+
+router.get("/:id/paymentmethods", (req, res, next) => {
+    try {
+        con.query("SELECT * from `Payment_Methods` where Customer_ID='" + req.params.id + "';", (err, result, fields) => {
+            return res.json(result);
+        })
+    }
+    catch (e) {
+        console.log(e);
+        return res.send("ERROR");
+    }
+});
 
 module.exports = router;
