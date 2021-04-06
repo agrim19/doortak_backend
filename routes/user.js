a84b3dbad8b476b37f55f68bf9ec53b4b1458ce9
Agrim <agrim19229@iiitd.ac.in>
2021-04-06 23:24:03 +0530

    update personal details route

--- routes/user.js
+++ routes/user.js
@@ -41,6 +41,19 @@ router.get("/:id", (req, res, next) => {
     try {
         con.query("Select * from Customer where Customer_ID = '" + req.params.id + "';", (err, result, fields) => {
             return res.json(result);
+        });
+    }
+    catch (e) {
+        console.log(e);
+        return res.send("ERROR");
+    }
+});
+
+router.post("/:id/update", (req, res, next) => {
+    const { Customer_ID, phoneNumber, Name } = req.body;
+    try {
+        con.query("update Customer set Phone_number ='" + phoneNumber + "',Name = '" + Name + "' where Customer_ID = '" + Customer_ID + "';", (err, result, fields) => {
+            return res.json(result);
         })
     }
     catch (e) {
