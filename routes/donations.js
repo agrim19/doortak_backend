8622eaa1cc3bfc74c478d81e04ff2a77f357b295
Agrim <agrim19229@iiitd.ac.in>
2021-04-09 18:50:30 +0530

    get donation dish details route

--- routes/donations.js
+++ routes/donations.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
 const con = require('../db');
 
+router.get("/:id/items", (req, res, next) => {
+    con.query("select * from Donation_Items where Donation_Number = '" + req.params.id + "';", async (err, result, fields) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    });
+});
+
 module.exports = router;
