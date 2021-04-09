55208e35724f333e40bb6fe7ec1b30cb4f827af3
Agrim <agrim19229@iiitd.ac.in>
2021-04-09 22:09:27 +0530

    update donation dishes route

--- routes/donations.js
+++ routes/donations.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const con = require('../db');
 
 router.get("/:id/items", (req, res, next) => {
-    con.query("select * from Donation_Items where Donation_Number = '" + req.params.id + "';", async (err, result, fields) => {
+    con.query("select * from Donation_Items, Dishes where Donation_Items.Dishes_ID = Dishes.Dishes_ID and Donation_Items.Donation_Number = '" + req.params.id + "';", async (err, result, fields) => {
         if (err) return res.json(err);
         return res.json(result);
     });
