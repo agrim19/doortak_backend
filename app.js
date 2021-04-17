f8e82e16487c00bfd4af6a4932ed28e9911fa6e6
Agrim <agrim19229@iiitd.ac.in>
2021-04-17 17:40:43 +0530

    fir production error in session -2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const session = require('express-session');
+const session = require('cookie-session');
 require('dotenv').config()
 
 app.use(express.urlencoded({ extended: false }));
